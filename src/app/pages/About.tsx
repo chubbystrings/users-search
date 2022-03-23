db8fbead299ea7d846670272e05fc912467dd475
dimola7 <didiogidan@gmail.com>
2022-03-23 17:37:45 +0100

    added about page content

--- src/app/pages/About.tsx
+++ src/app/pages/About.tsx
@@ -1,8 +1,20 @@
+import styled from "styled-components";
 
 const About = () => {
   return (
-    <div>Github finder</div>
+    <AboutPage>
+        <h1>Github finder</h1>
+        <p>This is an app to search for Github users.</p>
+    </AboutPage>
   )
 }
 
+const AboutPage = styled.div`
+    h1{
+        color: #fff;
+        font-size: 2rem;
+        font-weight: 600;
+    }
+`
+ 
 export default About
